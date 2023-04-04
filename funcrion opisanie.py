1d62c3819031fd8e8f61d2b878ff5c346a76c799
Emilia Palagacheva <71097698+epalagacheva@users.noreply.github.com>
2023-04-04 10:14:53 +0300

    Add files via upload

---
+++ funcrion opisanie.py
@@ -0,0 +1,23 @@
+
+
+
+
+
+def greet (name,surname,age):
+    #Display a something to users
+    print ("hello " + name +" "+ surname + "you are:" + str(age) + " old.")
+
+greet("Emi","Palagacheva", 20 ) # name="Emi"
+
+
+my_name="Emijyuygfti"
+my_surname="Palagachevadfsedfer"
+my_age=23210
+greet(my_name, my_surname,my_age) # name=my_name="EDmi"
+
+def pozdrav (ime):
+    return f'HI {ime}'
+pozdrav ("emka")
+
+pozdavi= pozdrav ("emka")
+print (pozdavi)
